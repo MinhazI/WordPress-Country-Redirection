e681d8f11b8e6d3ab5361776b507b2bca152a82b
Minhaz Irphan Mohamed <MinhazI@users.noreply.github.com>
2023-10-03 23:24:34 +0530

    Converted this to work with JavaScript IIFE instead of PHP

--- functions.php
+++ functions.php
@@ -2,63 +2,72 @@
 
 function country_selection_script()
 {
-    wp_enqueue_script('country-selection-script', get_stylesheet_directory_uri() . '/inc/country-selection.js', array('jquery'), '1.0', true);
+    wp_enqueue_script('country-selection-script', get_stylesheet_directory_uri() . '/inc/country-selection.js', array('jquery'), '1.0', false);
 }
+
 add_action('wp_enqueue_scripts', 'country_selection_script');
 
 /**
  * Check if the user needs to select a country.
  */
 
-function check_country_selection()
+// function check_country_selection()
+// {
+//     $currentURL = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+
+//     if (is_page('country-selection')) {
+//         // Do nothing on the country selection page
+//         return;
+//     }
+
+//     if (isset($_COOKIE['country'])) {
+//         $country = $_COOKIE['country'];
+
+//         // Check if the URL starts with "https://test.frokme.winauthority.net" (excluding "/uk")
+//         if (strpos($currentURL, 'https://test.frokme.winauthority.net') === 0 && strpos($currentURL, 'https://test.frokme.winauthority.net/uk') !== 0) {
+//             if ($country === 'lk') {
+//                 return;
+//             } else {
+//                 if (strpos($_SERVER['REQUEST_URI'], '/uk') === 0 && $country == 'lk') {
+//                     wp_safe_redirect('https://test.frokme.winauthority.net/' . ltrim($_SERVER['REQUEST_URI'], '/uk'));
+//                     exit;
+//                 } else if ($country === 'in') {
+//                     wp_safe_redirect('https://test.frokme.winauthority.net/uk' . $_SERVER['REQUEST_URI']);
+//                     exit;
+//                 } else {
+//                     wp_safe_redirect('https://test.frokme.winauthority.net' . $_SERVER['REQUEST_URI']);
+//                     exit;
+//                 }
+//             }
+//         } elseif (strpos($currentURL, 'https://test.frokme.winauthority.net/uk') === 0 && $country == 'in') {
+//             // User is on the "uk" site, and the cookie matches, so return
+//             return;
+//         } else {
+//             if ($country === 'lk') {
+//                 if (strpos($_SERVER['REQUEST_URI'], '/uk') === 0) {
+//                     wp_safe_redirect('https://test.frokme.winauthority.net/' . ltrim($_SERVER['REQUEST_URI'], '/uk'));
+//                     exit;
+//                 } else {
+//                     wp_safe_redirect('https://test.frokme.winauthority.net' . $_SERVER['REQUEST_URI']);
+//                     exit;
+//                 }
+//             } elseif ($country == 'in') {
+//                 wp_safe_redirect('https://test.frokme.winauthority.net/uk' . $_SERVER['REQUEST_URI']);
+//                 exit;
+//             }
+//         }
+//     } else {
+//         wp_safe_redirect(site_url() . '/country-selection?redirect=' . $_SERVER['REQUEST_URI']);
+//     }
+// }
+
+// // Hook into the template redirect action to check for country selection
+// add_action('template_redirect', 'check_country_selection');
+
+function add_noindex_nofollow_tags()
 {
-    $currentURL = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-
     if (is_page('country-selection')) {
-        // Do nothing on the country selection page
-        return;
-    }
-
-    if (isset($_COOKIE['country'])) {
-        $country = $_COOKIE['country'];
-
-        // Check if the URL starts with "https://test.frokme.winauthority.net" (excluding "/uk")
-        if (strpos($currentURL, 'https://test.frokme.winauthority.net') === 0 && strpos($currentURL, 'https://test.frokme.winauthority.net/uk') !== 0) {
-            if ($country === 'lk') {
-                return;
-            } else {
-                if (strpos($_SERVER['REQUEST_URI'], '/uk') === 0 && $country == 'lk') {
-                    wp_safe_redirect('https://test.frokme.winauthority.net/' . ltrim($_SERVER['REQUEST_URI'], '/uk'));
-                    exit;
-                } else if ($country === 'in') {
-                    wp_safe_redirect('https://test.frokme.winauthority.net/uk' . $_SERVER['REQUEST_URI']);
-                    exit;
-                } else {
-                    wp_safe_redirect('https://test.frokme.winauthority.net' . $_SERVER['REQUEST_URI']);
-                    exit;
-                }
-            }
-        } elseif (strpos($currentURL, 'https://test.frokme.winauthority.net/uk') === 0 && $country == 'in') {
-            // User is on the "uk" site, and the cookie matches, so return
-            return;
-        } else {
-            if ($country === 'lk') {
-                if (strpos($_SERVER['REQUEST_URI'], '/uk') === 0) {
-                    wp_safe_redirect('https://test.frokme.winauthority.net/' . ltrim($_SERVER['REQUEST_URI'], '/uk'));
-                    exit;
-                } else {
-                    wp_safe_redirect('https://test.frokme.winauthority.net' . $_SERVER['REQUEST_URI']);
-                    exit;
-                }
-            } elseif ($country == 'in') {
-                wp_safe_redirect('https://test.frokme.winauthority.net/uk' . $_SERVER['REQUEST_URI']);
-                exit;
-            }
-        }
-    } else {
-        wp_safe_redirect(site_url() . '/country-selection?redirect=' . $_SERVER['REQUEST_URI']);
+        echo '<meta name="robots" content="noindex,nofollow" />';
     }
 }
-
-// Hook into the template redirect action to check for country selection
-add_action('template_redirect', 'check_country_selection');
+add_action('wp_head', 'add_noindex_nofollow_tags');
--- inc/country-selection.js
+++ inc/country-selection.js
@@ -19,3 +19,149 @@ document.addEventListener('click', function (event) {
         }
     }
 });
+
+(function() {
+    function checkCountrySelection() {
+        var currentURL = window.location.protocol + "//" + window.location.host + window.location.pathname;
+
+        if (isPage('country-selection')) {
+            // Do nothing on the country selection page
+            return;
+        }
+
+        var countryCookie = getCookie('country');
+
+        if (countryCookie) {
+            var country = countryCookie;
+
+            if (currentURL.startsWith('https://test.frokme.winauthority.net') && !currentURL.startsWith('https://test.frokme.winauthority.net/uk')) {
+                if (country === 'lk') {
+                    return;
+                } else {
+                    if (window.location.pathname.startsWith('/uk') && country === 'lk') {
+                        window.location.href = 'https://test.frokme.winauthority.net' + window.location.pathname.substring(3);
+                    } else if (country === 'in') {
+                        window.location.href = 'https://test.frokme.winauthority.net/uk' + window.location.pathname;
+                    } else {
+                        window.location.href = 'https://test.frokme.winauthority.net' + window.location.pathname;
+                    }
+                }
+            } else if (currentURL.startsWith('https://test.frokme.winauthority.net/uk') && country === 'in') {
+                // User is on the "uk" site, and the cookie matches, so return
+                return;
+            } else {
+                if (country === 'lk') {
+                    if (window.location.pathname.startsWith('/uk')) {
+                        window.location.href = 'https://test.frokme.winauthority.net' + window.location.pathname.substring(3);
+                    } else {
+                        window.location.href = 'https://test.frokme.winauthority.net' + window.location.pathname;
+                    }
+                } else if (country === 'in') {
+                    window.location.href = 'https://test.frokme.winauthority.net/uk' + window.location.pathname;
+                }
+            }
+        } else {
+            window.location.href = siteURL() + '/country-selection?redirect=' + window.location.pathname;
+        }
+    }
+
+    function isPage(pageName) {
+        return window.location.pathname.includes(pageName);
+    }
+
+    function getCookie(cookieName) {
+        var name = cookieName + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var cookieArray = decodedCookie.split(';');
+        for (var i = 0; i < cookieArray.length; i++) {
+            var cookie = cookieArray[i];
+            while (cookie.charAt(0) === ' ') {
+                cookie = cookie.substring(1);
+            }
+            if (cookie.indexOf(name) === 0) {
+                return cookie.substring(name.length, cookie.length);
+            }
+        }
+        return "";
+    }
+
+    function siteURL() {
+        return window.location.protocol + "//" + window.location.host;
+    }
+
+    checkCountrySelection();
+})();
+
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     function checkCountrySelection() {
+//     var currentURL = window.location.protocol + "//" + window.location.host + window.location.pathname;
+
+//     if (isPage('country-selection')) {
+//         // Do nothing on the country selection page
+//         return;
+//     }
+
+//     var countryCookie = getCookie('country');
+
+//     if (countryCookie) {
+//         var country = countryCookie;
+
+//         if (currentURL.startsWith('https://test.frokme.winauthority.net') && !currentURL.startsWith('https://test.frokme.winauthority.net/uk')) {
+//             if (country === 'lk') {
+//                 return;
+//             } else {
+//                 if (window.location.pathname.startsWith('/uk') && country === 'lk') {
+//                     window.location.href = 'https://test.frokme.winauthority.net' + window.location.pathname.substring(3);
+//                 } else if (country === 'in') {
+//                     window.location.href = 'https://test.frokme.winauthority.net/uk' + window.location.pathname;
+//                 } else {
+//                     window.location.href = 'https://test.frokme.winauthority.net' + window.location.pathname;
+//                 }
+//             }
+//         } else if (currentURL.startsWith('https://test.frokme.winauthority.net/uk') && country === 'in') {
+//             // User is on the "uk" site, and the cookie matches, so return
+//             return;
+//         } else {
+//             if (country === 'lk') {
+//                 if (window.location.pathname.startsWith('/uk')) {
+//                     window.location.href = 'https://test.frokme.winauthority.net' + window.location.pathname.substring(3);
+//                 } else {
+//                     window.location.href = 'https://test.frokme.winauthority.net' + window.location.pathname;
+//                 }
+//             } else if (country === 'in') {
+//                 window.location.href = 'https://test.frokme.winauthority.net/uk' + window.location.pathname;
+//             }
+//         }
+//     } else {
+//         window.location.href = siteURL() + '/country-selection?redirect=' + window.location.pathname;
+//     }
+// }
+
+// function isPage(pageName) {
+//     return window.location.pathname.includes(pageName);
+// }
+
+// function getCookie(cookieName) {
+//     var name = cookieName + "=";
+//     var decodedCookie = decodeURIComponent(document.cookie);
+//     var cookieArray = decodedCookie.split(';');
+//     for (var i = 0; i < cookieArray.length; i++) {
+//         var cookie = cookieArray[i];
+//         while (cookie.charAt(0) === ' ') {
+//             cookie = cookie.substring(1);
+//         }
+//         if (cookie.indexOf(name) === 0) {
+//             return cookie.substring(name.length, cookie.length);
+//         }
+//     }
+//     return "";
+// }
+
+// function siteURL() {
+//     return window.location.protocol + "//" + window.location.host;
+// }
+
+// checkCountrySelection();
+
+// });
