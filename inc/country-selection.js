3ad749da02a700cdbfd4ae39f153c172fdf238ce
Minhaz Irphan Mohamed <MinhazI@users.noreply.github.com>
2024-05-29 13:01:19 +0530

    Updated the code

--- inc/country-selection.js
+++ inc/country-selection.js
@@ -1,98 +1,118 @@
-var indianWebsite = 'https://test.frokme.winauthority.net/uk';
-var indian = '/uk'
-var sriLankan = '/'
-var sriLankanWebsite = 'https://test.frokme.winauthority.net'
+var ukWebsite = "https://website.com/uk";
+var unitedKingdom = "/uk";
+var sriLankan = "/";
+var sriLankanWebsite = "https://website.com";
 
 // JavaScript code on your country selection page
-document.addEventListener('click', function (event) {
-    if (event.target.classList.contains('select-country')) {
-        var country = event.target.getAttribute('data-country');
-        document.cookie = "country=" + country + "; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/; priority=high;";
-        // Redirect the user to the appropriate website based on their selection
-        // Get the query string part of the current URL
-        var queryString = window.location.search;
+document.addEventListener("click", function (event) {
+  if (event.target.classList.contains("select-country")) {
+    var country = event.target.getAttribute("data-country");
+    document.cookie =
+      "country=" +
+      country +
+      "; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/; priority=high;";
+    // Redirect the user to the appropriate website based on their selection
+    // Get the query string part of the current URL
+    var queryString = window.location.search;
 
-        // Create a URLSearchParams object to parse the query string
-        var urlParams = new URLSearchParams(queryString);
+    // Create a URLSearchParams object to parse the query string
+    var urlParams = new URLSearchParams(queryString);
 
-        // Access the value of the "redirect" parameter
-        var redirectValue = urlParams.get('redirect');
-        if(redirectValue && redirectValue.length > 0){
-            window.location.href = country === 'in' ? indianWebsite + '/' + redirectValue.replace(indian, '') : sriLankanWebsite + '/' + redirectValue.replace(indian, '');
-        } else {
-            window.location.href = country === 'in' ? indianWebsite + '/' : sriLankanWebsite + '/';
-        }
+    // Access the value of the "redirect" parameter
+    var redirectValue = urlParams.get("redirect");
+    if (redirectValue && redirectValue.length > 0) {
+      window.location.href =
+        country === "uk"
+          ? ukWebsite + "/" + redirectValue.replace(unitedKingdom, "")
+          : sriLankanWebsite + "/" + redirectValue.replace(unitedKingdom, "");
+    } else {
+      window.location.href =
+        country === "uk" ? ukWebsite + "/" : sriLankanWebsite + "/";
     }
+  }
 });
 
-(function() {
-    function checkCountrySelection() {
-        var currentURL = window.location.protocol + "//" + window.location.host + window.location.pathname;
+(function () {
+  function checkCountrySelection() {
+    var currentURL =
+      window.location.protocol +
+      "//" +
+      window.location.host +
+      window.location.pathname;
 
-        if (isPage('country-selection')) {
-            // Do nothing on the country selection page
-            return;
-        }
+    if (isPage("country-selection")) {
+      // Do nothing on the country selection page
+      return;
+    }
 
-        var countryCookie = getCookie('country');
+    var countryCookie = getCookie("country");
 
-        if (countryCookie) {
-            var country = countryCookie;
+    if (countryCookie) {
+      var country = countryCookie;
 
-            if (currentURL.startsWith(sriLankanWebsite) && !currentURL.startsWith(indianWebsite)) {
-                if (country === 'lk') {
-                    return;
-                } else {
-                    if (window.location.pathname.startsWith(indian) && country === 'lk') {
-                        window.location.href = sriLankanWebsite + window.location.pathname.substring(3);
-                    } else if (country === 'in') {
-                        window.location.href = indianWebsite + window.location.pathname;
-                    } else {
-                        window.location.href = sriLankanWebsite + window.location.pathname;
-                    }
-                }
-            } else if (currentURL.startsWith(indianWebsite) && country === 'in') {
-                // User is on the "uk" site, and the cookie matches, so return
-                return;
-            } else {
-                if (country === 'lk') {
-                    if (window.location.pathname.startsWith(indian)) {
-                        window.location.href = sriLankanWebsite + window.location.pathname.substring(3);
-                    } else {
-                        window.location.href = sriLankanWebsite + window.location.pathname;
-                    }
-                } else if (country === 'in') {
-                    window.location.href = indianWebsite + window.location.pathname;
-                }
-            }
+      if (
+        currentURL.startsWith(sriLankanWebsite) &&
+        !currentURL.startsWith(ukWebsite)
+      ) {
+        if (country === "lk") {
+          return;
         } else {
-            window.location.href = siteURL() + '/country-selection?redirect=' + window.location.pathname;
+          if (
+            window.location.pathname.startsWith(unitedKingdom) &&
+            country === "lk"
+          ) {
+            window.location.href =
+              sriLankanWebsite + window.location.pathname.substring(3);
+          } else if (country === "uk") {
+            window.location.href = ukWebsite + window.location.pathname;
+          } else {
+            window.location.href = sriLankanWebsite + window.location.pathname;
+          }
         }
-    }
-
-    function isPage(pageName) {
-        return window.location.pathname.includes(pageName);
-    }
-
-    function getCookie(cookieName) {
-        var name = cookieName + "=";
-        var decodedCookie = decodeURIComponent(document.cookie);
-        var cookieArray = decodedCookie.split(';');
-        for (var i = 0; i < cookieArray.length; i++) {
-            var cookie = cookieArray[i];
-            while (cookie.charAt(0) === ' ') {
-                cookie = cookie.substring(1);
-            }
-            if (cookie.indexOf(name) === 0) {
-                return cookie.substring(name.length, cookie.length);
-            }
+      } else if (currentURL.startsWith(ukWebsite) && country === "uk") {
+        // User is on the "uk" site, and the cookie matches, so return
+        return;
+      } else {
+        if (country === "lk") {
+          if (window.location.pathname.startsWith(unitedKingdom)) {
+            window.location.href =
+              sriLankanWebsite + window.location.pathname.substring(3);
+          } else {
+            window.location.href = sriLankanWebsite + window.location.pathname;
+          }
+        } else if (country === "uk") {
+          window.location.href = ukWebsite + window.location.pathname;
         }
-        return "";
+      }
+    } else {
+      window.location.href =
+        siteURL() + "/country-selection?redirect=" + window.location.pathname;
     }
+  }
 
-    function siteURL() {
-        return window.location.protocol + "//" + window.location.host;
+  function isPage(pageName) {
+    return window.location.pathname.includes(pageName);
+  }
+
+  function getCookie(cookieName) {
+    var name = cookieName + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var cookieArray = decodedCookie.split(";");
+    for (var i = 0; i < cookieArray.length; i++) {
+      var cookie = cookieArray[i];
+      while (cookie.charAt(0) === " ") {
+        cookie = cookie.substring(1);
+      }
+      if (cookie.indexOf(name) === 0) {
+        return cookie.substring(name.length, cookie.length);
+      }
     }
+    return "";
+  }
 
-    checkCountrySelection();
+  function siteURL() {
+    return window.location.protocol + "//" + window.location.host;
+  }
+
+  checkCountrySelection();
 })();
--- inc/old-solution.js
+++ inc/old-solution.js
@@ -1,72 +1,88 @@
-document.addEventListener("DOMContentLoaded", function() {
-    function checkCountrySelection() {
-    var currentURL = window.location.protocol + "//" + window.location.host + window.location.pathname;
+document.addEventListener("DOMContentLoaded", function () {
+  function checkCountrySelection() {
+    var currentURL =
+      window.location.protocol +
+      "//" +
+      window.location.host +
+      window.location.pathname;
 
-    if (isPage('country-selection')) {
-        // Do nothing on the country selection page
-        return;
+    if (isPage("country-selection")) {
+      // Do nothing on the country selection page
+      return;
     }
 
-    var countryCookie = getCookie('country');
+    var countryCookie = getCookie("country");
 
     if (countryCookie) {
-        var country = countryCookie;
+      var country = countryCookie;
 
-        if (currentURL.startsWith('https://test.frokme.winauthority.net') && !currentURL.startsWith('https://test.frokme.winauthority.net/uk')) {
-            if (country === 'lk') {
-                return;
-            } else {
-                if (window.location.pathname.startsWith('/uk') && country === 'lk') {
-                    window.location.href = 'https://test.frokme.winauthority.net' + window.location.pathname.substring(3);
-                } else if (country === 'in') {
-                    window.location.href = 'https://test.frokme.winauthority.net/uk' + window.location.pathname;
-                } else {
-                    window.location.href = 'https://test.frokme.winauthority.net' + window.location.pathname;
-                }
-            }
-        } else if (currentURL.startsWith('https://test.frokme.winauthority.net/uk') && country === 'in') {
-            // User is on the "uk" site, and the cookie matches, so return
-            return;
+      if (
+        currentURL.startsWith("https://website.com") &&
+        !currentURL.startsWith("https://website.com/uk")
+      ) {
+        if (country === "lk") {
+          return;
         } else {
-            if (country === 'lk') {
-                if (window.location.pathname.startsWith('/uk')) {
-                    window.location.href = 'https://test.frokme.winauthority.net' + window.location.pathname.substring(3);
-                } else {
-                    window.location.href = 'https://test.frokme.winauthority.net' + window.location.pathname;
-                }
-            } else if (country === 'in') {
-                window.location.href = 'https://test.frokme.winauthority.net/uk' + window.location.pathname;
-            }
+          if (window.location.pathname.startsWith("/uk") && country === "lk") {
+            window.location.href =
+              "https://website.com" + window.location.pathname.substring(3);
+          } else if (country === "in") {
+            window.location.href =
+              "https://website.com/uk" + window.location.pathname;
+          } else {
+            window.location.href =
+              "https://website.com" + window.location.pathname;
+          }
         }
+      } else if (
+        currentURL.startsWith("https://website.com/uk") &&
+        country === "in"
+      ) {
+        // User is on the "uk" site, and the cookie matches, so return
+        return;
+      } else {
+        if (country === "lk") {
+          if (window.location.pathname.startsWith("/uk")) {
+            window.location.href =
+              "https://website.com" + window.location.pathname.substring(3);
+          } else {
+            window.location.href =
+              "https://website.com" + window.location.pathname;
+          }
+        } else if (country === "in") {
+          window.location.href =
+            "https://website.com/uk" + window.location.pathname;
+        }
+      }
     } else {
-        window.location.href = siteURL() + '/country-selection?redirect=' + window.location.pathname;
+      window.location.href =
+        siteURL() + "/country-selection?redirect=" + window.location.pathname;
     }
-}
+  }
 
-function isPage(pageName) {
+  function isPage(pageName) {
     return window.location.pathname.includes(pageName);
-}
+  }
 
-function getCookie(cookieName) {
+  function getCookie(cookieName) {
     var name = cookieName + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
-    var cookieArray = decodedCookie.split(';');
+    var cookieArray = decodedCookie.split(";");
     for (var i = 0; i < cookieArray.length; i++) {
-        var cookie = cookieArray[i];
-        while (cookie.charAt(0) === ' ') {
-            cookie = cookie.substring(1);
-        }
-        if (cookie.indexOf(name) === 0) {
-            return cookie.substring(name.length, cookie.length);
-        }
+      var cookie = cookieArray[i];
+      while (cookie.charAt(0) === " ") {
+        cookie = cookie.substring(1);
+      }
+      if (cookie.indexOf(name) === 0) {
+        return cookie.substring(name.length, cookie.length);
+      }
     }
     return "";
-}
+  }
 
-function siteURL() {
+  function siteURL() {
     return window.location.protocol + "//" + window.location.host;
-}
+  }
 
-checkCountrySelection();
-
+  checkCountrySelection();
 });
